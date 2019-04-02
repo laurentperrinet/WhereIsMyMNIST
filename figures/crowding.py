848dcc856048c86f7f82e7e851b62a50ae192bcb
dauce <dauce@port-dauce.ec-m.fr>
2019-04-02 22:15:03 +0200

    crowding script 2

--- figures/crowding.py
+++ figures/crowding.py
@@ -1,3 +1,5 @@
+## SCRIPT
+
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,18 +11,18 @@ from retina import Display, Retina
 from whatWhere import WhatWhere
 
 args = init(filename='../data/2019-03-29')
-FIC_NAME = '../data/2019-03-29-crowding'
+FIC_NAME = '../data/2019-04-02-crowding-contrast'
 result = {}
 
 for period in range(1,21):
     args.sf_0 = 1/period
     result[period] = {} 
-    for prop in np.arange(0.25,2.25,0.25):
-        args.B_sf = prop/period
+    for contrast in (0.5, 0.7):
+        args.contrast = contrast
         whatWhere = WhatWhere(args, save = False)
         whatWhere.train()
         data_test, label_test = next(iter(whatWhere.loader_test)) 
         acc = whatWhere.pred_accuracy(data_test, label_test)
-        print('sf_0 : %.2f, B_sf : %.2f, acc : %.2f'%(args.sf_0, args.B_sf, acc))
+        print(f'sf_0 : %.2f, B_sf : %.2f, acc : %.2f'%(args.sf_0, args.B_sf, acc))
         result[period][prop] = acc
         np.save(FIC_NAME + '-result.npy', result)
