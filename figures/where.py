22b11e2bee7160732ed8511fda3dfcb7ed2044c0
dauce <dauce@port-dauce.ec-m.fr>
2019-04-05 12:41:05 +0200

    correction where

--- figures/where.py
+++ figures/where.py
@@ -81,7 +81,7 @@ class Where():
             from retina import get_data_loader        
             # SAVING DATASET
             if args.verbose: print('Creating training dataset')
-            retina_data, _, accuracy_colliculus, _ = self.generate_data(self.args.train_batch_size, train=True, fullfield=False, batch_load = False)
+            retina_data, _, accuracy_colliculus, _ = self.generate_data(self.args.train_batch_size, train=True, fullfield=False, batch_load = batch_load)
             # create your dataset, see dev/2019-03-18_precomputed dataset.ipynb
             self.loader_train = DataLoader(TensorDataset(retina_data, accuracy_colliculus), batch_size=args.minibatch_size)
             if save:
@@ -95,7 +95,7 @@ class Where():
             self.loader_test  = torch.load(filename_dataset)
         else:
             if args.verbose: print('Creating testing dataset')
-            retina_data, retina_fullfield_data, accuracy_colliculus, label = self.generate_data(self.args.test_batch_size, train=False, fullfield=True, batch_load = False)
+            retina_data, retina_fullfield_data, accuracy_colliculus, label = self.generate_data(self.args.test_batch_size, train=False, fullfield=True, batch_load = batch_load)
             # create your dataset, see dev/2019-03-18_precomputed dataset.ipynb
             self.loader_test = DataLoader(TensorDataset(retina_data, retina_fullfield_data, accuracy_colliculus, label), batch_size=args.test_batch_size)
             if save:
@@ -135,10 +135,10 @@ class Where():
         if batch_load:
             if train:
                 size = self.args.train_batch_size
-                print('train dataset, size = ', size)
+                if self.args.verbose: print('train dataset, size = ', size)
             else:
                 size = self.args.test_batch_size
-                print('test dataset, size = ', size)
+                if self.args.verbose: print('test dataset, size = ', size)
             loader_full = get_data_loader(batch_size=size, train=train, mean=self.args.mean, std=self.args.std, seed=self.args.seed+1)
             data, label = next(iter(loader_full))
             for i in range(size):
