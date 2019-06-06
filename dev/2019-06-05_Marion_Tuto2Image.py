d8a1bf79630dc9ac845204bf941a9205149b2c79
Marion <marion-fi@hotmail.fr>
2019-06-06 09:41:57 +0200

    Script Hydra

---
+++ dev/2019-06-05_Marion_Tuto2Image.py
@@ -0,0 +1,40 @@
+# https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
+# Finetuning torchvision models
+
+
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
+
+
+
+# Top level data directory. Here we assume the format of the directory conforms
+#   to the ImageFolder structure
+data_dir = "./data/hymenoptera_data"
+
+# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
+model_name = "squeezenet"
+
+# Number of classes in the dataset
+num_classes = 2
+
+# Batch size for training (change depending on how much memory you have)
+batch_size = 8
+
+# Number of epochs to train for
+num_epochs = 15
+
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = True
