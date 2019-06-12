a5121a1f98d67b5deb1d446721ece099b97bf8e8
Marion <marion-fi@hotmail.fr>
2019-06-12 13:50:00 +0200

    modif valeur offset_std

--- dev/robust_what_train.py
+++ dev/robust_what_train.py
@@ -15,7 +15,7 @@ from robust_what import WhatShift, WhatBackground, WhatNet, WhatTrainer, What, t
 from main import init
 args = init(filename='../data/2019-06-05')
 
-args.epochs = 10
+args.epochs = 60
 args.noise = 1
 args.save_model = True
 
