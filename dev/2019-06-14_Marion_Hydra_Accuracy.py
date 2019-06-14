2c92e5a81de460e3849bd58ae7094679eef1e241
Marion <marion-fi@hotmail.fr>
2019-06-14 16:48:53 +0200

    test sur Hydra

--- dev/2019-06-14_Marion_Hydra_Accuracy.py
+++ dev/2019-06-14_Marion_Hydra_Accuracy.py
@@ -6,6 +6,10 @@ sys.path.append("../figures")
 
 from robust_what import WhatShift, WhatBackground, WhatNet, WhatTrainer, What, train, test, MNIST
 
+from main import init
+args = init(filename='../data/2019-06-12')
+args
+
 model = torch.load("../data/MNIST_cnn_robust_what_0.1_0.1_1.0_0.7_5epoques_2019-06-14_15h00.pt")
 transform = transforms.Compose([
     WhatShift(args, i_offset=None, j_offset=None),
