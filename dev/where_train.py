4ce4999ce19cd2554b1b72c402cf14b3d2324d92
Marion <marion-fi@hotmail.fr>
2019-07-02 10:03:45 +0200

    entrainement reseau where

--- dev/where_train.py
+++ dev/where_train.py
@@ -8,8 +8,8 @@ import datetime
 from main import init
 args = init(filename='../data/2019-06-12')
 
-args.epochs = 60
-args.save_model = True
+#args.epochs = 60
+args.save_model = False
 
 where = Where(args, what_model="MNIST_cnn_robust_what_0.1_0.1_1.0_0.7_5epoques_2019-06-19_11h50.pt")
 # c'est le reseau robust what qui donne l'accuracy map etendue de tests2sur2.ipynb
