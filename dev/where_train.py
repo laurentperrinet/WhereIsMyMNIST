0b7b77668acf7c534cc4b68910af42a457080613
Marion <marion-fi@hotmail.fr>
2019-07-02 09:32:14 +0200

    entrainement reseau where

--- dev/where_train.py
+++ dev/where_train.py
@@ -4,9 +4,9 @@ sys.path.append("../figures")
 from where_copie import RetinaTransform, WhereNet, CollTransform, MNIST, Normalize, WhereTrainer, Where
 
 from main import init
-args = init(filename='../data/2019-06-13')
+args = init(filename='../data/2019-06-12')
 
-args.epochs = 60
+args.epochs = 2
 args.save_model = True
 
 where = Where(args)
