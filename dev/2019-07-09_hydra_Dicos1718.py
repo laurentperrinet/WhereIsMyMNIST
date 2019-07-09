aaa7cd63dbbe41e5bb2c130bd236285cc991b7d6
Marion <marion-fi@hotmail.fr>
2019-07-09 16:04:46 +0200

    Script Hydra (RAM ordi insuffisante)

---
+++ dev/2019-07-09_hydra_Dicos1718.py
@@ -0,0 +1,11 @@
+from main import init
+args = init(filename='../data/2019-07-08')
+args.train_batch_size = 1000
+args.test_batch_size = 207
+args.contrast = 0.5
+args.N_azimuth = 24
+args.N_eccentricity = 12
+args
+
+from retina_copie import Retina
+retina = Retina(args)
