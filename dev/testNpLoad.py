9d7f400626c64d6841a61705a897cc1ce03a94eb
Marion <marion-fi@hotmail.fr>
2019-07-10 15:38:28 +0200

    test np load

--- dev/testNpLoad.py
+++ dev/testNpLoad.py
@@ -1,5 +1,6 @@
+pip install numpy==1.16.1
 import numpy as np
 
-dico = np.load("../tmp/retina_6_24_12_2_1.41_1718_dico.npy").item()
+dico = np.load("../tmp/retina_6_24_12_2_1.41_1718_dico.npy", allow_pickle=True).item()
 
 print("ok")
