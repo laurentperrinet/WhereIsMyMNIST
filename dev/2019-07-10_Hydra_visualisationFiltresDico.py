40b232b780a29012d9fc136b7e62301738803488
Marion <marion-fi@hotmail.fr>
2019-07-11 14:16:44 +0200

    Parametres Dico 1718 modifies

--- dev/2019-07-10_Hydra_visualisationFiltresDico.py
+++ dev/2019-07-10_Hydra_visualisationFiltresDico.py
@@ -7,8 +7,7 @@ args = init(filename='../data/2019-07-08')
 args.train_batch_size = 1000
 args.test_batch_size = 207
 args.contrast = 0.5
-args.N_azimuth = 24
-args.N_eccentricity = 12
+
 
 from retina_copie import Retina
 retina = Retina(args)
