094b5790509ca6a195eca9702633dff2428ca66d
Marion <marion-fi@hotmail.fr>
2019-07-12 10:23:26 +0200

    davantage d'excentricités

--- dev/2019-07-10_Hydra_EssaiInverseDicoChicago.py
+++ dev/2019-07-10_Hydra_EssaiInverseDicoChicago.py
@@ -11,6 +11,7 @@ from main import init
 args = init(filename='../data/2019-07-08')
 args.train_batch_size = 1000
 args.test_batch_size = 207
+args.N_eccentricity = 12
 args.contrast = 0.5
 
 from retina_chicago import Retina
