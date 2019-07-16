953bcde051cad9ab1c211891ed01b060031a10a5
Marion <marion-fi@hotmail.fr>
2019-07-16 10:44:59 +0200

    sauvegarde résultats "ratés" avant suppression

--- dev/retina_chicago.py
+++ dev/retina_chicago.py
@@ -243,8 +243,7 @@ class Retina:
 
 
     def init_retina_dico(self):
-        # filename = '../tmp/retina' + self.get_suffix() + '_dico.npy'
-        filename = '../tmp/retina' + self.get_suffix() + '_dicoXXX_filtres_20_20.npy'
+        filename = '../tmp/retina' + self.get_suffix() + '_dico.npy'
         if self.args.verbose: print(filename)
         try:
             self.retina_dico = np.load(filename, allow_pickle=True).item()
