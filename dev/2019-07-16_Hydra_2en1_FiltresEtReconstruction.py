7fc1e55c299ff44bc0b08bf7ca9c47d57edb21bd
Marion <marion-fi@hotmail.fr>
2019-07-17 09:55:30 +0200

    essai dico 18 ecc

--- dev/2019-07-16_Hydra_2en1_FiltresEtReconstruction.py
+++ dev/2019-07-16_Hydra_2en1_FiltresEtReconstruction.py
@@ -12,9 +12,9 @@ from main import init
 args = init(filename='../data/2019-07-08')
 args.train_batch_size = 1000
 args.test_batch_size = 207
-args.N_eccentricity = 12
+args.N_eccentricity = 18
 args.contrast = 0.5
-nom = "_24_12"
+nom = "_{}_{}".format(args.N_azimuth, args.N_eccentricity)
 
 from retina_chicago import Retina
 retina = Retina(args)
